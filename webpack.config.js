320a8d38ab4f76900d1813b96c331f3de5e1110d
Andrey Ageev <andrey-ageev-mail@yandex.ru>
2022-09-26 21:53:33 +0600

    Added HTML Webpack plugin.

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,9 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const mode = process.env.NODE_ENV || 'development';
-
 const devMode = mode === 'development';
-
 const target = devMode ? 'web' : 'browserslist';
-
 const devtool = devMode ? 'source-map' : undefined;
 
 module.exports = {
@@ -17,5 +15,10 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         clean: true,
         filename: '[name].[contenthash].js'
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'src', 'index.html')
+        }),
+    ],
 };
