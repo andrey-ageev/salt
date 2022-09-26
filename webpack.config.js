a08cb7b0ac61996ef6a59f05adb025847c7b1d5c
Andrey Ageev <andrey-ageev-mail@yandex.ru>
2022-09-26 23:35:51 +0600

    Added MiniCssExtractPlugin and Sass loader.

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const mode = process.env.NODE_ENV || 'development';
 const devMode = mode === 'development';
@@ -20,13 +21,24 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, 'src', 'index.html')
         }),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css',
+        })
     ],
     module: {
         rules: [
             {
                 test: /\.html$/i,
                 loader: 'html-loader', 
-            }
-        ]
-    }
+            },
+            {
+                test: /\.(c|sa|sc)ss$/i,
+                use: [
+                    devMode ? "style-loader" : MiniCssExtractPlugin.loader, 
+                    "css-loader",
+                    'sass-loader'
+                ],
+            },
+        ],
+    },
 };
