eec6b42e59646ff7ffa1c2a555c061d32c1e6050
Andrey Ageev <andrey-ageev-mail@yandex.ru>
2022-10-06 01:08:32 +0600

    Added settings for working with pictures, "image-webpack-loader" is installed.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import './index.html';
 import './index.scss';
+import cubes from './img/cubes.jpg';
 import { mult, sum } from './libs/modules/calc';
 
+const img = new Image();
+img.src = cubes;
 
 console.log(mult(5, 6));
 console.log(sum(8, 4));
