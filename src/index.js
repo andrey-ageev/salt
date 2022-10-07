a1149193931330a704f95badc2ad2411a0f951a5
Andrey Ageev <andrey-ageev-mail@yandex.ru>
2022-10-07 23:36:38 +0600

    Added files from the prototype.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './index.html';
 import './index.scss';
-import cubes from './img/cubes.jpg';
+// import cubes from './img/cubes.jpg';
 import { mult, sum } from './libs/modules/calc';
 
 const img = new Image();
